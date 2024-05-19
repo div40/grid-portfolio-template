4dfe0be1eda75cf2de832fe33bc156aac0aa07f2
div40 <vidh40@gmail.com>
2024-05-19 23:50:06 +0530

    added loading in animations

--- src/components/Bento.tsx
+++ src/components/Bento.tsx
@@ -1,19 +1,30 @@
+"use client";
 import AboutBlock from "./AboutBlock";
 import HeaderBlock from "./HeaderBlock";
 import LocationBlock from "./LocationBlock";
+import Logo from "./Logo";
 import MessageListBlock from "./MessageListBlock";
 import SocialsBlock from "./SocialsBlock";
+import { motion } from "framer-motion";
 
 function Bento() {
   return (
-    <div className="min-h-screen bg-zinc-900 px-4 py-12 text-zinc-50">
-      <div className="mx-auto grid max-w-4xl grid-flow-dense grid-cols-12 gap-4 text-balance">
+    <div className="min-h-screen bg-zinc-900 px-4 py-8 text-zinc-50">
+      <Logo />
+      <motion.div
+        initial="initial"
+        animate="animate"
+        transition={{
+          staggerChildren: 0.05,
+        }}
+        className="mx-auto grid max-w-4xl grid-flow-dense grid-cols-12 gap-4 text-balance"
+      >
         <HeaderBlock />
         <SocialsBlock />
         <AboutBlock />
         <LocationBlock />
         <MessageListBlock />
-      </div>
+      </motion.div>
     </div>
   );
 }
--- src/components/Block.tsx
+++ src/components/Block.tsx
@@ -9,6 +9,24 @@ type BlockProps = {
 export default function Block({ className, ...rest }: BlockProps) {
   return (
     <motion.div
+      variants={{
+        initial: {
+          scale: 0.5,
+          y: 50,
+          opacity: 0,
+        },
+        animate: {
+          scale: 1,
+          y: 0,
+          opacity: 1,
+        },
+      }}
+      transition={{
+        type: "spring",
+        mass: 3,
+        stiffness: 400,
+        damping: 50,
+      }}
       className={twMerge(
         "col-span-4 rounded-lg border border-zinc-700 bg-zinc-800 p-6",
         className
